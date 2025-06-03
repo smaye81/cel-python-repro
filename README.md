e4be3d1364a9087e036e927c8c5c1a116e2d2d9c
Steve Ayers <sayers@buf.build>
2025-06-03 09:52:52 -0400

    Tests

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 `pipenv --python python3 sync --dev`
+
 `pipenv run python3 run.py`
 
--- run.py
+++ run.py
@@ -2,18 +2,28 @@ import celpy
 from celpy import celtypes
 
 env = celpy.Environment(runner_class=celpy.InterpretedRunner)
+
+# Test field access
+# Note that v0.2.0 raised the below error
+
+# TypeError: no such overload: StringType('foo') <class 'celpy.celtypes.StringType'> != CELEvalError(*("no such member in mapping: 'b'", <class 'KeyError'>, None), tree='dyn(this).b') <class 'celpy.evaluation.CELEvalError'>
+
 ast = env.compile("dyn(this).b == 'foo'")
 prog = env.program(ast)
 
-bindings = {
+result = prog.evaluate({
     "this": celtypes.MapType({"a": "foo"})
-}
-result = prog.evaluate(bindings)
+})
 print("Result: {}".format(result))
 
+# Test has
+ast = env.compile("has(this, 'b')")
+prog = env.program(ast)
 
-# 0.2.0
+has_b = prog.evaluate({
+    "this": celtypes.MapType({"a": "foo"})
+})
+print("Has b: {}".format(has_b))
 
-# TypeError: no such overload: StringType('foo') <class 'celpy.celtypes.StringType'> != CELEvalError(*("no such member in mapping: 'b'", <class 'KeyError'>, None), tree='dyn(this).b') <class 'celpy.evaluation.CELEvalError'>
 
 
